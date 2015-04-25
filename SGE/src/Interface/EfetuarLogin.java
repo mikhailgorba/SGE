1084da98be0f9234aba6c9a0e6d306494f0175bd
Mikhail <Mikhail@192.168.1.8>
2015-04-25 20:47:08 -0300

    Criando a classe Fornecedor

--- SGE/src/Interface/EfetuarLogin.java
+++ SGE/src/Interface/EfetuarLogin.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Interface;
 
-/**
- *
- * @author Mikhail
- */
 public class CadastroUsuario extends javax.swing.JInternalFrame {
 
     /**
