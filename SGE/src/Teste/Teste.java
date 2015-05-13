36d771ba8972390e07b7dad66e872fbeecd6b7a2
Mikhail <Mikhail@Mikhail-PC>
2015-05-13 18:25:40 -0300

    Populando testes

--- SGE/src/Teste/Teste.java
+++ SGE/src/Teste/Teste.java
@@ -2,13 +2,97 @@ package Teste;
 
 import static org.junit.Assert.*;
 
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
 public class Teste {
 
 	@Test
 	public void test() {
+		
+		
+		JOptionPane.showMessageDialog(null, "Hey Brothers");
+		
+		
 		fail("Not yet implemented");
 	}
 
-}
+	
+	@Test
+	public void testeIconedeErro() {
+		
+		String mensagem = "mensagem de texto";
+		String titulo = "Aqui fica o titulo'";
+		int icone = 1;
+		
+		
+		JOptionPane.showMessageDialog(null, mensagem, titulo, icone);
+		
+	}
+	
+	
+	@Test
+	public void testandoSwitch() {
+		
+		int diaDaSemana ;
+		diaDaSemana = 3;
+		
+		switch (diaDaSemana) {
+		
+		case 1:
+		
+		System.out.println("Domingo");
+		
+		break;
+		
+		case 2:
+		
+		System.out.println("Segunda-feira");
+		
+		break;
+	
+		case 3:
+		
+		System.out.println("Terça-feira");
+		
+		break;
+		
+		case 4:
+		
+		System.out.println("Quarta-feira");
+		
+		break;
+		
+		case 5:
+		
+		System.out.println("Quinta-feira");
+		
+		break;
+		
+		case 6:
+		
+		System.out.println("Sexta-feira");
+		
+		break;
+		
+		case 7:
+		
+		System.out.println("Sábado");
+	
+		break;
+	
+		default:
+		
+		System.out.println("Este não é um dia válido!");
+	
+		}
+		
+		}
+		
+	
+		
+	}
+	
+	
+
