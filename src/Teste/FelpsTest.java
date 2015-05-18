84d348cd18b3530ef1e0bfec6f053371c0628e8e
Mikhail <Mikhail@Mikhail-PC>
2015-05-17 21:50:08 -0300

    Teste exemplo

--- src/Teste/FelpsTest.java
+++ src/Teste/FelpsTest.java
@@ -4,212 +4,30 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Interface.ComprasMateriaPrima;
 import Objetos.Fornecedor;
+import Sistema.Sistema;
 
 public class FelpsTest {
 
 	@Test
 	public void test() {
-		public String pesquisar(String qtdAluminio, String qtdParafuso, String qtdBorracha, String qtdCabo, String qtdPlastico) {
-
-			String strRetorno = "";
-			Integer iQtdAluminio=0;
-			Integer iQtdParafuso=0;
-			Integer iQtdBorracha=0;
-			Integer iQtdPlastico=0;
-			Integer iQtdCabo=0;
-			boolean isTudoCerto = true;
-
-
-			try {
-				iQtdAluminio = Integer.parseInt(qtdAluminio);
-			} catch (Exception e) {
-				isTudoCerto = false;
-				strRetorno = "Quantidade de aluminio invalida\n";
-			}
-
-			try {
-				iQtdParafuso = Integer.parseInt(qtdParafuso);
-			} catch (Exception e) {
-				isTudoCerto = false;
-				strRetorno += "Quantidade de parafuso invalida\n";
-			}
-
-			try {
-				iQtdBorracha = Integer.parseInt(qtdBorracha);
-			} catch (Exception e) {
-				isTudoCerto = false;
-				strRetorno += "Quantidade de borracha invalida\n";
-			}
-
-			try {
-				iQtdPlastico = Integer.parseInt(qtdPlastico);
-			} catch (Exception e) {
-				isTudoCerto = false;
-				strRetorno += "Quantidade de plastico invalida\n";
-			}
-
-			try {
-				iQtdCabo = Integer.parseInt(qtdCabo);
-			} catch (Exception e) {
-				isTudoCerto = false;
-				strRetorno += "Quantidade de cabo invalida\n";
-			}
-
-			if (isTudoCerto)
-			{
-
-				Fornecedor melhorFornecedorAluminio=null;
-				Fornecedor melhorFornecedorPlastico=null;
-				Fornecedor melhorFornecedorBorracha=null;
-				Fornecedor melhorFornecedorCabo=null;
-				Fornecedor melhorFornecedorParafuso=null;
-
-				/*			private double precoAluminio;
-				private double precoParafuso;
-				private double precoCabo;
-				private double precoBorracha;
-				private double precoPlastico;
-				 */
-
-				
-				double a= 0.0, b=0.0, c=0.0, d=0.0, e=0.0, f=0.0;
-				double valorPedido;
-				double menorValor=9999999;
-				
-				//aluminio
-				if (iQtdAluminio>0) {
-					for (int i = 0; i < fornecedores.size(); i++) 
-					{
-						Fornecedor fornecedorAux = fornecedores.get(i);
-						double precoAluminio = fornecedorAux.getPrecoAluminio();
-						double precoFrete = fornecedorAux.getFrete();
-
-						valorPedido = ((iQtdAluminio*precoAluminio)+precoFrete);
-						if (valorPedido < menorValor){
-							melhorFornecedorAluminio = fornecedorAux;
-							menorValor = valorPedido;
-						}
-					}
-					a = menorValor;
-					strRetorno = "Melhor Aluminio = " + melhorFornecedorAluminio.getNome()+" "+ menorValor +"\n";
-				}
-
-				if (iQtdParafuso > 0) {
-					// parafuso
-					menorValor=9999999;
-					for (int i = 0; i < fornecedores.size(); i++) 
-					{
-						Fornecedor fornecedorAux = fornecedores.get(i);
-						double precoParafuso = fornecedorAux.getPrecoParafuso();
-						double precoFrete = fornecedorAux.getFrete();
-
-						if (melhorFornecedorAluminio != null && fornecedorAux.getNome() == melhorFornecedorAluminio.getNome())
-							valorPedido = iQtdParafuso * precoParafuso;
-						else
-							valorPedido = ( iQtdParafuso * precoParafuso ) + precoFrete;
-
-						if (valorPedido < menorValor){
-							melhorFornecedorParafuso = fornecedorAux;
-							menorValor = valorPedido;
-						}
-					}
-					b = menorValor;
-					strRetorno += "Melhor Parafuso = " + melhorFornecedorParafuso.getNome()+" "+ menorValor +"\n";
-				}
-
-				if (iQtdCabo > 0) {
-
-					// cabo
-					menorValor=9999999;
-					for (int i = 0; i < fornecedores.size(); i++) 
-					{
-						Fornecedor fornecedorAux = fornecedores.get(i);
-						double precoCabo = fornecedorAux.getPrecoCabo();
-						double precoFrete = fornecedorAux.getFrete();
-
-						if (	(melhorFornecedorAluminio != null && fornecedorAux.getNome() == melhorFornecedorAluminio.getNome()) ||
-								(melhorFornecedorParafuso != null && fornecedorAux.getNome() == melhorFornecedorParafuso.getNome()))
-						{
-							valorPedido = iQtdCabo * precoCabo;
-						}
-						else
-							valorPedido = ( iQtdCabo * precoCabo ) + precoFrete;
-
-						if (valorPedido < menorValor){
-							melhorFornecedorCabo = fornecedorAux;
-							menorValor = valorPedido;
-						}
-					}
-					c = menorValor;
-					strRetorno += "Melhor Cabo = " + melhorFornecedorCabo.getNome()+" "+ menorValor +"\n";
-				}
-
-				if (iQtdBorracha > 0) {
-
-					// Borracha
-					menorValor=9999999;
-					for (int i = 0; i < fornecedores.size(); i++) 
-					{
-						Fornecedor fornecedorAux = fornecedores.get(i);
-						double precoBorracha = fornecedorAux.getPrecoBorracha();
-						double precoFrete = fornecedorAux.getFrete();
-
-						if (	(melhorFornecedorAluminio != null && fornecedorAux.getNome() == melhorFornecedorAluminio.getNome()) ||
-								(melhorFornecedorParafuso != null && fornecedorAux.getNome() == melhorFornecedorParafuso.getNome()) ||
-								(melhorFornecedorCabo != null && fornecedorAux.getNome() == melhorFornecedorCabo.getNome())
-							)
-							valorPedido = iQtdBorracha * precoBorracha;
-						else
-							valorPedido = ( iQtdBorracha * precoBorracha ) + precoFrete;
-
-						if (valorPedido < menorValor){
-							melhorFornecedorBorracha = fornecedorAux;
-							menorValor = valorPedido;
-						}
-
-					}
-					d = menorValor;
-					strRetorno += "Melhor Borracha = " + melhorFornecedorBorracha.getNome()+" "+ menorValor +"\n";
-				}
-
-				if (iQtdPlastico > 0) {
-
-					// Plastico
-					menorValor=9999999;
-					for (int i = 0; i < fornecedores.size(); i++) 
-					{
-						Fornecedor fornecedorAux = fornecedores.get(i);
-						double precoPlastico = fornecedorAux.getPrecoPlastico();
-						double precoFrete = fornecedorAux.getFrete();
-
-						if (	(melhorFornecedorAluminio != null && fornecedorAux.getNome() == melhorFornecedorAluminio.getNome()) ||
-								(melhorFornecedorParafuso != null && fornecedorAux.getNome() == melhorFornecedorParafuso.getNome()) ||
-								(melhorFornecedorCabo != null && fornecedorAux.getNome() == melhorFornecedorCabo.getNome()) ||
-								(melhorFornecedorBorracha != null && fornecedorAux.getNome() == melhorFornecedorBorracha.getNome())
-							)
-							valorPedido = iQtdPlastico * precoPlastico;
-						else
-							valorPedido = ( iQtdPlastico * precoPlastico ) + precoFrete;
-
-						if (valorPedido < menorValor){
-							melhorFornecedorPlastico = fornecedorAux;
-							menorValor = valorPedido;
-						}
-					}//fimFor
-					e = menorValor;
-					strRetorno += "Melhor Plastico = " + melhorFornecedorPlastico.getNome()+" "+ menorValor +"\n";
-				}
-				
-				f = (a+b+c+d+e);
-				strRetorno += "O valor total da compra é de: R$ " + f;
-				
-			}//fimIf
-
-			return strRetorno;
+		String qtdAluminio, qtdParafuso, qtdBorracha, qtdCabo, qtdPlastico;
+		// Criei cenario
+		qtdAluminio = "1";
+		qtdParafuso = "2";
+		qtdBorracha = "2";
+		qtdCabo = "2";
+		qtdPlastico = "2";
 		
-		fail("Not yet implemented");
-		}	
+		
+		
+		// Executo um procedimento neste cenario
+		Sistema cp = new Sistema();
+		String resultado = cp.pesquisar(qtdAluminio, qtdParafuso, qtdBorracha, qtdCabo, qtdPlastico);
+		
+		//verifico se a saida é a esperada
+		assertEquals("Melhor Aluminio = Cap. Corp. 25.0\nMelhor Parafuso = Doublemt 40.0\nMelhor Cabo = Cap. Corp. 40.0\nMelhor Borracha = Wayne Inc. 60.0\nMelhor Plastico = Wayne Inc. 20.0\nO valor total da compra é de: R$ 185.0", resultado);
 	}
 
 }
