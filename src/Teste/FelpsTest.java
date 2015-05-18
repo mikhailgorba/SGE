783a181af253f6f54eb59c0561c7bb6e7dbb7461
Mikhail <Mikhail@Mikhail-PC>
2015-05-17 22:31:12 -0300

    mais testes

--- src/Teste/FelpsTest.java
+++ src/Teste/FelpsTest.java
@@ -2,6 +2,8 @@ package Teste;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
+
 import org.junit.Test;
 
 import Interface.ComprasMateriaPrima;
@@ -10,24 +12,29 @@ import Sistema.Sistema;
 
 public class FelpsTest {
 
-	@Test
+	 @Test
 	public void test() {
-		String qtdAluminio, qtdParafuso, qtdBorracha, qtdCabo, qtdPlastico;
+		//String qtdAluminio, qtdParafuso, qtdBorracha, qtdCabo, qtdPlastico;
 		// Criei cenario
-		qtdAluminio = "1";
-		qtdParafuso = "2";
-		qtdBorracha = "2";
-		qtdCabo = "2";
-		qtdPlastico = "2";
+	//	qtdAluminio = "1";
+		//qtdParafuso = "2";
+		//qtdBorracha = "2";
+	//	qtdCabo = "2";
+		//qtdPlastico = "2";
 		
 		
 		
 		// Executo um procedimento neste cenario
 		Sistema cp = new Sistema();
-		String resultado = cp.pesquisar(qtdAluminio, qtdParafuso, qtdBorracha, qtdCabo, qtdPlastico);
+		EntradaDeDados EntradaDeDados = new EntradaDeDados();
+	//	String resultado = cp.pesquisar(qtdAluminio, qtdParafuso, qtdBorracha, qtdCabo, qtdPlastico);
 		
 		//verifico se a saida é a esperada
-		assertEquals("Melhor Aluminio = Cap. Corp. 25.0\nMelhor Parafuso = Doublemt 40.0\nMelhor Cabo = Cap. Corp. 40.0\nMelhor Borracha = Wayne Inc. 60.0\nMelhor Plastico = Wayne Inc. 20.0\nO valor total da compra é de: R$ 185.0", resultado);
+	//	assertEquals("Melhor Aluminio = Cap. Corp. 25.0\nMelhor Parafuso = Doublemt 40.0\nMelhor Cabo = Cap. Corp. 40.0\nMelhor Borracha = Wayne Inc. 60.0\nMelhor Plastico = Wayne Inc. 20.0\nO valor total da compra é de: R$ 185.0", resultado);
 	}
 
+	
+	        
+				
+	
 }
