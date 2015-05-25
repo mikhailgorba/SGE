601b7197c037f1ec37c130116dc1b4b08f0a451d
Mikhail <Mikhail@Mikhail-PC>
2015-05-25 20:24:15 -0300

    l

--- src/Interface/TelaPrincipal.java
+++ src/Interface/TelaPrincipal.java
@@ -167,7 +167,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         	JOptionPane.showMessageDialog(null, "Não");
         }
         
-        
+        dispose();
         
     }                                          
 
