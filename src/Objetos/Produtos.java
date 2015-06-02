002067d9b51e3a85a9ace03874cec3735dda476e
Mikhail <Mikhail@Mikhail-PC>
2015-06-01 22:28:25 -0300

    Merge

--- src/Objetos/Produtos.java
+++ src/Objetos/Produtos.java
@@ -6,15 +6,9 @@ import java.io.FileReader;
 public class Produtos {
 
 	
-<<<<<<< HEAD
-	private String  nome = "Aluminio";
-
-	public String getNome() {
-=======
 	private String nome  = "Selecione";
 		
 		public String getNome() {
->>>>>>> branch 'master' of https://github.com/mikhailgorba/SGE
 		return nome;
 	}
 
